061ad116dc4bd929724157ef31d459e3b01c9236
Helder Menegatti <heldermenegatti720@gmail.com>
2021-09-05 22:27:02 -0300

    changing the color of the registration box and starting user registration view

--- initial/views.py
+++ initial/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request):
     template_name = "initial/index.html"
     return render(request, template_name)
+
+
+def sing_up(request):
+    template_name = "user/singup.html"
+    if request.method == "POST":
+        email = request.POST['email']
+        print(email)
+    return render(request, template_name)
