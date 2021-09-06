f0e57257736d47c70f1278dd151db17ee34d9ea6
HelderMenegatti <heldermenegatti720@gmail.com>
2021-09-06 18:29:19 -0300

    Inserting user field and bringing information from templates to view.

--- initial/views.py
+++ initial/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -6,9 +7,20 @@ def index(request):
     return render(request, template_name)
 
 
-def sing_up(request):
+def sign_up(request):
     template_name = "user/singup.html"
     if request.method == "POST":
         email = request.POST['email']
-        print(email)
+        username = request.POST['username']
+        password= request.POST['password']
+        print("O EMAIL ENVIADO FOI", email)
+        print("O username é", username)
+        print("A senha enviada foi", password)
+
+        user = User.objects.create_user(username, email, password)
+        print(user)
+        print(user)
+        print(user)
+        print(user)
+        print(user)
     return render(request, template_name)
